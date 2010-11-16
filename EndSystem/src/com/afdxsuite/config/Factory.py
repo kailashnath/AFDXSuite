9fd5310f969f95e8931670e7c8d57c01a4eb6643
Kailashnath <knr413@gmail.com>
2010-11-16 22:23:21 +0530

    Revert "Made minor changes to Factory"
    This reverts commit 39dc75112502c5b6adf5762de7a9f4dcea528d0f.

--- EndSystem/src/com/afdxsuite/config/Factory.py
+++ EndSystem/src/com/afdxsuite/config/Factory.py
@@ -3,15 +3,25 @@ from com.afdxsuite.config.parsers.icdparser import CONFIG_ENTRIES, PORT_SAMPLING
 
 PROCESSED_PORTS = {}
 
-def __get_vl(vlId, type):
+def __get_vl(vlId, dst_ip = None, dst_udp = None, port_id = None,
+             type = ICD_INPUT_VL):
 
     entries = CONFIG_ENTRIES[type]
     for entry in entries:
         if type != ICD_ICMP:
-            if entry.vl_id == vlId:
-                return entry
-        else:
-            if entry.
+
+            if entry.vl_id == vlId: 
+                if port_id == None:
+#                    print entry.ip_dst, dst_ip, entry.udp_dst, dst_udp
+                    if entry.ip_dst == dst_ip \
+                        and entry.udp_dst == dst_udp:
+                        return entry
+                else:
+                    if type == ICD_INPUT_VL and entry.RX_AFDX_port_id == port_id:
+                        return entry
+                    elif entry.tx_AFDX_port_id == port_id:
+                        return entry
+    return None
 
 def __get_port(portId, type):
 
@@ -19,7 +29,15 @@ def __get_port(portId, type):
     port_attr_name = "tx_AFDX_port_id" if type == ICD_OUTPUT_VL \
                                         else "RX_AFDX_port_id"
     for entry in entries:
+
         if getattr(entry, port_attr_name) == portId:
+            return entry
+    return None
+
+def __get_sap_port(sapSrcPort, type):
+    entries = CONFIG_ENTRIES[type]
+    for entry in entries:
+        if entry.udp_src == sapSrcPort:
             return entry
     return None
 
@@ -56,12 +74,13 @@ def WRITE(afdxPortId, payload):
     __set_port(port, afdxPortId, ICD_OUTPUT_VL)
     return port
 
-def WRITE_Sap(sapPortId, payload, ipDest, udpDest):
-    port = __get_port(sapPortId, ICD_OUTPUT_VL)
+def WRITE_Sap(sapSrcPort, payload, ipDest, udpDest):
+    port = __get_sap_port(sapSrcPort, ICD_OUTPUT_VL)
+
     setattr(port, 'payload', payload)
     port.ip_dst = ipDest
     port.udp_dst = udpDest
-    __set_port(port, sapPortId, ICD_OUTPUT_VL)
+    __set_port(port, port.tx_AFDX_port_id, ICD_OUTPUT_VL)
     return port
 
 def READ(portId):
@@ -82,14 +101,15 @@ def READ_Sampling(samplingPortId):
 def READ_Queuing(queuingPortId):
     return READ(queuingPortId)
 
-def GET_InputVl(vlId):
-    return __get_vl(vlId, ICD_INPUT_VL)
+def GET_InputVl(vlId, dst_ip, dst_udp):
+    return __get_vl(vlId, dst_ip = dst_ip, dst_udp = int(dst_udp), 
+                    type = ICD_INPUT_VL)
 
-def GET_ICMPVl(vlId):
+def GET_ICMPVl(vlId, ip_dst, udp_dst):
     return __get_vl(vlId, ICD_ICMP)
 
-def GET_OutputVl(vlId):
-    return __get_vl(vlId, ICD_OUTPUT_VL)
+def GET_OutputVl(vlId, afdxPortId):
+    return __get_vl(vlId, port_id = int(afdxPortId), type = ICD_OUTPUT_VL)
 
 def GET_Port_Input(portId):
     return __get_port(portId, ICD_INPUT_VL)
