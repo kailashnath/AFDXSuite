344902371f41bd7ca52ca5181607a3e9db1c4aab
Kailash <kailashnathreddy@ymail.com>
2011-03-28 09:32:09 +0530

    Made changes to stop_capture.sh, used pkill as was facing problem with cut -d

--- Scripts/stop_capture.sh
+++ Scripts/stop_capture.sh
@@ -3,5 +3,6 @@
 chmod -R 777 ./captures/
 #ps -e -o pid,command | grep $1 | gawk -F " " '{print "kill -9 ", $1;}' > /tmp/cap_pids
 
-kill -9 `ps -e -o pid,command | grep dumpcap | cut -d" " -f2`
+#kill -9 `ps -e -o pid,command | grep dumpcap | cut -d" " -f1`
+pkill dumpcap
 
